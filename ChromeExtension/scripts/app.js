d5cb52bab2fb8148948728f8ba674bcdd3862d70
Tomas Filip <tomfilip107@gmail.com>
2017-04-09 22:14:01 +0200

    Amend to last commit

--- ChromeExtension/scripts/app.js
+++ ChromeExtension/scripts/app.js
@@ -52,5 +52,41 @@
 		return data;
 	});
 
+	app.run(['$rootScope', '$window', 'srvAuth',
+      function ($rootScope, $window, sAuth) {
+
+          $rootScope.user = {};
+
+          $window.fbAsyncInit = function () {
+              FB.init({
+                  appId: '290412921380719',
+                  status: true,
+                  cookie: true,
+                  xfbml: true,
+                  version: 'v2.4'
+              });
+          };
+
+          (function (d) {
+              // load the Facebook javascript SDK
+
+              var js,
+              id = 'facebook-jssdk',
+              ref = d.getElementsByTagName('script')[0];
+
+              if (d.getElementById(id)) {
+                  return;
+              }
+
+              js = d.createElement('script');
+              js.id = id;
+              js.async = true;
+              js.src = "//connect.facebook.net/en_US/sdk.js";
+
+              ref.parentNode.insertBefore(js, ref);
+
+          }(document));
+
+      }]);
 })();
 
